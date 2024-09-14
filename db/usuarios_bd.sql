d0fae95ebed7a0693a0755ffb4d4c5906eeee346
HigorAz <higor.azevedo2010@gmail.com>
2024-09-13 21:56:13 -0300

    V1 - Todas as rotas/funções construídas
    Faltam só as interfaces para finalizar, todas as rotas e funções foram criadas com base nos requerimentos passados pelo professor

---
+++ db/usuarios_bd.sql
@@ -0,0 +1,11 @@
+PRAGMA encoding = "UTF-8";
+
+CREATE TABLE IF NOT EXISTS usuarios (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  login varchar(100) NOT NULL,
+  senha varchar(100) NOT NULL,
+  nome_real varchar(100) not null,
+  status boolean default TRUE,
+  created TIMESTAMP DEFAULT (DATETIME(CURRENT_TIMESTAMP, '-3 hours')),
+  modified TIMESTAMP DEFAULT (DATETIME(CURRENT_TIMESTAMP, '-3 hours'))
+);
