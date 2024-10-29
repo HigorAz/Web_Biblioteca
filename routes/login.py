c38f4784776e014daacd10a7686773657cfbb1e5
HigorAz <higor.azevedo2010@gmail.com>
2024-10-29 00:40:47 -0300

    Added reset password to the code

--- routes/login.py
+++ routes/login.py
@@ -1,17 +1,77 @@
 import sqlite3
-from flask import flash, redirect, render_template, request, session, url_for
+from dotenv import load_dotenv
+from flask import flash, redirect, render_template, request, session, url_for, current_app
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadSignature
 from . import bp
 from db.database import get_db
 from datetime import datetime
 import pytz
 import bcrypt
 import re
+import os
+
+load_dotenv()
+
+mail = Mail()
+s = URLSafeTimedSerializer(os.getenv("SECRET_KEY"))
 
 now = datetime.now(pytz.timezone('America/Sao_Paulo')).strftime('%Y-%m-%d %H:%M:%S')
 
 def validar_email(email):
     regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
     return re.match(regex, email) is not None
+
+@bp.route('/forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'POST':
+        email = request.form['email']
+        
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM usuarios WHERE login = ?", (email,))
+        usuario = cursor.fetchone()
+        db.close()
+
+        if not usuario:
+            flash('E-mail não encontrado no sistema.', category='danger')
+            return render_template('forgot_password.html')
+
+        token = s.dumps(email, salt='password_recovery')
+        msg = Message('Redefinição de senha', sender=os.getenv("MAIL_USERNAME"), recipients=[email])
+        link = url_for('routes.reset_password', token=token, _external=True)
+        msg.body = f'Clique no link para redefinir sua senha: {link}'
+        mail.send(msg)
+
+        flash('Um link de recuperação de senha foi enviado para o seu email', category='success')
+        return redirect(url_for('routes.login'))
+
+    return render_template('forgot_password.html')
+
+
+@bp.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    try:
+        email = s.loads(token, salt='password_recovery', max_age=3600)
+    except SignatureExpired:
+        return '<h1>O link de redefinição de senha expirou</h1>'
+    except BadSignature:
+        return '<h1>Token inválido</h1>'
+
+    if request.method == 'POST':
+        new_password = request.form['password']
+        hashed_password = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
+        
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("UPDATE usuarios SET senha = ? WHERE login = ?", (hashed_password, email))
+        db.commit()
+        db.close()
+
+        flash('Sua senha foi redefinida com sucesso!', category='success')
+        return redirect(url_for('routes.login'))
+    
+    return render_template('reset_password.html')
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
